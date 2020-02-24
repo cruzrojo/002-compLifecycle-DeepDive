c752c1bbbe8085d5a33d8bd9daee5b67fade89b9
cruzrojo <burkerich@gmail.com>
2020-02-24 11:10:15 -0700

    third commit. Changes to files.

--- src/components/app.js
+++ src/components/app.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import Discussion from './discussion';
+import Rules from './rules';
+import Workflow from './workflow';
 
 export default class App extends Component {
   render() {
@@ -6,6 +9,9 @@ export default class App extends Component {
       <div className='app'>
         <h1>Burke Rich's Component Lifecycle Deep Dive Page</h1>
         <h2>React Skeleton...click...clack...BOOOOO!!!</h2>
+        <Discussion />
+        <Rules />
+        <Workflow />
       </div>
     );
   }
