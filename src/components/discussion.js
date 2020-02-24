f91019f2b89897d74df26f8ece1620bd048c7cba
cruzrojo <burkerich@gmail.com>
2020-02-24 12:16:26 -0700

    final commit for this project. Made the changes to discussion.js

--- src/components/discussion.js
+++ src/components/discussion.js
@@ -3,9 +3,32 @@ import React, { Component } from "react";
 export default class Discussion extends Component {
     constructor() {
         super()
+
+        this.state = {
+            pageTitle: "Discussion",
+            currentTime: String(new Date())
+        };
     }
     
-    render () {
-        return <div>Discussion...</div>;
+    componentDidMount() {
+        this.liveTime = setInterval(() => {
+            console.log("New chat message");
+        
+            this.setState({ currentTime: String(new Date())});
+          }, 1000);
+        }
+
+    componentWillUnmount() {
+        clearInterval(this.liveTime);
+      }
+
+    render() {
+        const { pageTitle, currentTime } = this.state;
+        return (
+          <div>
+            <h1>{pageTitle}</h1>
+            <div>{currentTime}</div>
+          </div>
+        )
+      }
     }
-}
